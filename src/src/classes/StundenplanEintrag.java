2d9cadec1f2a26ea98c99b423d3859ed631a1204
vmai <vmai@inovex.de>
2019-03-26 12:31:09 +0100

    Eintrag, but dont know the right struct

--- src/src/classes/StundenplanEintrag.java
+++ src/src/classes/StundenplanEintrag.java
@@ -1,4 +1,29 @@
 package classes;
 
 public class StundenplanEintrag {
+
+  static int id;
+  static String name;
+  static boolean zufaellig;
+
+  // KP wie der Aufbau der String aussehen soll
+
+  public StundenplanEintrag(int id, String name, boolean zufaellig) {
+    this.id = id;
+    this.name = name;
+    this.zufaellig = zufaellig;
+  }
+
+  public static String leererEintragToString(int id) {
+    return "Leer" + id;
+  }
+
+  public static String printAufbau() {
+    return name + ", " + id + ", " + zufaellig;
+  }
+
+  public String toString() {
+    return name + ", " + id + ", " + zufaellig;
+  }
+
 }
