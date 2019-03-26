3bde7b7adb9c0b1e767b4b72d07412aaca3bc90e
vmai <vmai@inovex.de>
2019-03-26 17:54:25 +0100

    Abgabetermin

--- src/src/classes/Abgabetermin.java
+++ src/src/classes/Abgabetermin.java
@@ -1,4 +1,10 @@
 package classes;
 
 public class Abgabetermin {
+
+  public static void main(String... args) {
+    Stundenplan plan = new Stundenplan(Integer.parseInt(args[0]));
+    String[] seperated = args[1].split("-");
+    System.out.println(seperated);
+  }
 }
